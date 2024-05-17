da15bdff93f94760da2f9cce8111c58decb0ab9f
hiro <155888748+iskillhiro@users.noreply.github.com>
2024-05-17 11:41:32 +0000

    s

--- bot.py
+++ bot.py
@@ -1,3 +1,26 @@
+from flask import Flask
+from flask import request
+from threading import Thread
+import time
+import requests
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+  return "I'm alive"
+
+
+def run():
+  app.run(host='0.0.0.0', port=80)
+
+
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
+
+
 import telebot
 from telebot import types
 from facts import get_random_fact
